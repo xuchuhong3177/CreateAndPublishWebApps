db6141c45cb1f22b28bcf10c63ad9607734f576f
Rebecca Xu <xuchuhong3177@gmail.com>
2024-08-24 18:58:22 -0700

    Initial Commit

---
+++ webAppwithStreamlit.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Our first Streamlit app')
+st.subheader('Introducing Streamlit in Automate Everything with Python')
+st.write(''' This is our first Web App
+Enjoy it!
+''')
+
